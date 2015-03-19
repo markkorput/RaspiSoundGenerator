23a7f53a1e832942eeb31fc5e73a0bbe3e2b873f
Mark vd Korput <dr.theman@gmail.com>
2015-03-19 09:44:56 +0000

    final pins config (hopefully)

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ class StrpConfig:
   def __init__(self):
     self.rotaryA = 17 #26 #15
     self.rotaryB = 18 #21 #14
-    self.touchOutPins = [22, 20] #[18,22,27,6,19]
-    self.touchInPins = [23, 26] #[17,23,13,12,16] 
+    self.touchOutPins = [27,23,5,13] #[18,22,27,6,19]
+    self.touchInPins = [22,24,6,19] #[17,23,13,12,16] 
     self.rotaryFreqStep = 0.0 # how much the rotary affects frequency (make negative to reverse the direction)
     self.rotaryGainStep = 0.0 # how much the rotary affects the gain (make negative to reverse the direction)
     self.rotaryPosStep = 0.1 # how much the rotary affects the frequency sine-wave affector (make negative to reverse the direction)
